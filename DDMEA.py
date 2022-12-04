609f940bd0674c1d56b86ca8d478698e6eeae007
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-12-04 15:14:14 -0700

    Update DDMEA.py

--- DDMEA.py
+++ DDMEA.py
@@ -66,10 +66,9 @@ elif sect == "Booking":
 	name = st.text_input(label="What's your name?", placeholder="Full Name - So I know what to call you")
 	email = st.text_input(label="What's your email?", placeholder="Email (name@company.extension) - for contact information")
 	
-	if "@" not in email:
+	if "@" not in email and email != "":
 		st.markdown(f'<h4 style="color:\'white\';font-size:18px;">Invalid email address</h4>', unsafe_allow_html=True)
 	else:
-		
 		cn = st.text_input(label="Card Number:", placeholder="Credit/Debit Card Number - for the payment")
 		
 		try:
