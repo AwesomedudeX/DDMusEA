3b40c022f893b3956978a4c1e128af0bb6e4dd74
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-12-05 17:23:15 -0700

    Update DDMEA.py

--- DDMEA.py
+++ DDMEA.py
@@ -106,72 +106,74 @@ elif sect == "Booking":
 				cn = int(cn)
 				pin = st.text_input(label="PIN:", placeholder="PIN - for confirmation (this won't be saved)")
 				loc = st.text_input(label="Address:", placeholder="Address - so I know where to be")
-			except:
-				cn = str(cn)
-				st.markdown(f'<h4 style="color:\'white\';font-size:18px;">Invalid card number</h4>', unsafe_allow_html=True)
-	if name != "" and email != "" and cn != "" and pin != "" and loc != "":
+			
+				if name != "" and email != "" and cn != "" and pin != "" and loc != "":
 
-		t = st.selectbox("Type:", ["Party", "Wedding"])
-		
-		dl = [i for i in range(1, 29)]
-		
-		m = st.selectbox("Month:", ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
-		y = int(dt.now().strftime("%Y"))
-
-		if m in ["January", "March", "May", "July", "September", "November", "December"]:
-			dl += [29, 30, 31]
-		elif m == "February":
-			if y % 4 == 0:
-				dl += [29]
-		else:
-			dl += [29, 30]
-
-
-		d = st.selectbox("Day:", dl)
-
-		apm = st.selectbox("Time Period:", ["AM", "PM"])
-		dur = st.selectbox("Party Duration (Hours):", [1, 2, 3])
-
-		if apm == "AM":
-			if dur == 1:
-				s = st.selectbox("Start time:", [7, 8, 9, 10, 11])
-			if dur == 2:
-				s = st.selectbox("Start time:", [7, 8, 9, 10, 11])
-			if dur == 3:
-				s = st.selectbox("Start time:", [7, 8, 9, 10, 11])
-		
-		elif apm == "PM":
-			if dur == 1:
-				s = st.selectbox("Start time:", [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])	
-			if dur == 2:
-				s = st.selectbox("Start time:", [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])	
-			if dur == 3:
-				s = st.selectbox("Start time:", [12, 1, 2, 3, 4, 5, 6, 7, 8, 9])	
-
-		e = s+dur
-
-		if e > 11 and apm == "PM":
-			eapm = "AM"
-		elif e > 12 and apm == "AM":
-			e -= 12
-			eapm = "PM"
-		else:
-			eapm = apm
-
-		st.write(f"{t} time: {m} {d}, {y} - from {s}{apm} to {e}{eapm} (MST)")
-
-		if t == "Party":
-			st.write(f"Party cost: ${120*dur}")
-		elif t == "Wedding":
-			st.write(f"Wedding cost: ${150*dur}")
-
-		submit = st.button("Book Event")
-
-		if submit:
-			st.markdown(f'<h3 style="color:\'0055FF\';font-size:20px;">{t} booked successfully!</h3>', unsafe_allow_html=True)
-			st.write("(Reselect this section in the navigation box to book another event)")
-
-
+					t = st.selectbox("Type:", ["Party", "Wedding"])
+					
+					dl = [i for i in range(1, 29)]
+					
+					m = st.selectbox("Month:", ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+					y = int(dt.now().strftime("%Y"))
+			
+					if m in ["January", "March", "May", "July", "September", "November", "December"]:
+						dl += [29, 30, 31]
+					elif m == "February":
+						if y % 4 == 0:
+							dl += [29]
+					else:
+						dl += [29, 30]
+			
+			
+					d = st.selectbox("Day:", dl)
+			
+					apm = st.selectbox("Time Period:", ["AM", "PM"])
+					dur = st.selectbox("Party Duration (Hours):", [1, 2, 3])
+			
+					if apm == "AM":
+						if dur == 1:
+							s = st.selectbox("Start time:", [7, 8, 9, 10, 11])
+						if dur == 2:
+							s = st.selectbox("Start time:", [7, 8, 9, 10, 11])
+						if dur == 3:
+							s = st.selectbox("Start time:", [7, 8, 9, 10, 11])
+					
+					elif apm == "PM":
+						if dur == 1:
+							s = st.selectbox("Start time:", [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])	
+						if dur == 2:
+							s = st.selectbox("Start time:", [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])	
+						if dur == 3:
+							s = st.selectbox("Start time:", [12, 1, 2, 3, 4, 5, 6, 7, 8, 9])	
+			
+					e = s+dur
+			
+					if e > 11 and apm == "PM":
+						eapm = "AM"
+					elif e > 12 and apm == "AM":
+						e -= 12
+						eapm = "PM"
+					else:
+						eapm = apm
+			
+					st.write(f"{t} time: {m} {d}, {y} - from {s}{apm} to {e}{eapm} (MST)")
+			
+					if t == "Party":
+						st.write(f"Party cost: ${120*dur}")
+					elif t == "Wedding":
+						st.write(f"Wedding cost: ${150*dur}")
+			
+					submit = st.button("Book Event")
+			
+					if submit:
+						st.markdown(f'<h3 style="color:\'0055FF\';font-size:20px;">{t} booked successfully!</h3>', unsafe_allow_html=True)
+						st.write("(Reselect this section in the navigation box to book another event)")
+			
+						
+						except:
+							cn = str(cn)
+							st.markdown(f'<h4 style="color:\'white\';font-size:18px;">Invalid card number</h4>', unsafe_allow_html=True)
+			
 elif sect == "About":
 
 	st.markdown(f'<h1 style="color:#FF0000;font-size:40px;">About Dave:</h1>', unsafe_allow_html=True)
