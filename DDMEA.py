a9ab3281b2fe1a7220e37b985c72fe028146113d
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-12-06 16:02:28 -0700

    Update DDMEA.py

--- DDMEA.py
+++ DDMEA.py
@@ -145,16 +145,17 @@ elif sect == "Products":
 		st.markdown(f'<h3 style="color:\'0055FF\';font-size:20px;">{product}</h3>', unsafe_allow_html=True)
 		st.image("Spotlight.jpg")
 		cn = st.text_input(label="", placeholder="Credit/Debit Card Number")
-		pin = st.text_input(label="", placeholder="PIN")
 		address = st.text_input(label="", placeholder="Address")
-		if cn != "" and pin != "":
+		if cn != "" and len(cn) > 8 and len(cn) < 14:
+
+			pin = st.text_input(label="", placeholder="PIN")
+
 			try:
 
 				cn = int(cn)
 				pin = int(pin)
 
-				if len(str(pin)) > 2 and len(str(pin)) < 7 and len(str(cn)) > 8 and len(str(cn)) < 14:
-
+				if len(str(pin)) > 2 and len(str(pin)) < 7:
 					if address != "":
 						if st.button("Order 🗒"):
 							st.markdown(f'<h3 style="color:\'0055FF\';font-size:20px;">Your purchase has been completed!</h3>', unsafe_allow_html=True)
